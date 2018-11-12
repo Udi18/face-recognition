cbcbb6f091edab6a7facf3f905cd0ba9dac1a442
Udi Levy <webdevudi@gmail.com>
2018-11-12 13:22:06 -0600

    trying to make it connect

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://git.heroku.com/whispering-falls-68900.git/register', {
+    fetch('https://git.heroku.com/whispering-falls-68900.gitgit/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
